b454a3fa7f27fa47f996ef1b00dfcb601f232304
katyamatya21 <katyamatya21@gmail.com>
2018-10-11 01:52:32 +0300

    Отдаём содержимое файла events.json

--- app/index.js
+++ app/index.js
@@ -1,21 +1,33 @@
 const express = require('express');
 const format = require('./format-time.js');
+const fs = require('fs');
 
 const app = express();
-
 const startTime = Date.now();
-let currentTime = null;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.all('/status', function (req, res, next) {
-  currentTime = Date.now();
+  const currentTime = Date.now();
   const timeDelta = (currentTime - startTime) / 1000;
   const stringTime = format.formatTime(timeDelta);
 
   res.send(stringTime);
+  next();
+});
+
+app.all('/api/events', function (req, res, next) {
+
+  if (!fs.existsSync('data/events.json')) {
+    console.log('The file does not exist! Please, provide data/events.json!');
+    return;
+  }
+
+  const inputData = JSON.parse(fs.readFileSync('data/events.json', 'utf8'));
+
+  res.send(inputData);
   next();
 });
 
